c580f7a683e32485bd9584358ef24099276ac253
felixthecat17 <felixjohnguevarra@gmail.com>
2021-11-27 12:48:55 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>complete responsive adventure & tour website design</title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    
+<!-- header section starts  -->
+
+<header class="header">
+
+    <a href="#" class="logo"> <i class="fas fa-user-tie"></i> robzleesantor. </a>
+
+    <nav class="navbar">
+        <div id="nav-close" class="fas fa-times"></div>
+        <a href="#home">home</a>
+        <a href="#contact_me">contact me</a>
+    </nav>
+
+    <div class="icons">
+        <div id="menu-btn" class="fas fa-bars"></div>
+    </div>
+
+</header>
+
+<!-- header section ends -->
+
+<!-- about section starts  -->
+
+<section class="about" id="home">
+
+    <div class="image">
+        <img src="images/img.jpg" alt="">
+    </div>
+
+    <div class="content">
+        <h3>Hi! I am Roberlyn <span class="lastname">SANTOR</span></h3>
+        <p>I am 23 years old and currently taking Bachelor of Science in Information Technology.</p>
+        <p><em>"Whoever said that the definition of insanity is doing the same thing over and over again and expecting different results has obviously never had to reboot a computer."</em></p>
+        <p><em>—William Petersen</em></p>
+        <a href="#" class="btn">read more</a>
+    </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- search form  -->
+
+<div class="search-form">
+
+    <div id="close-search" class="fas fa-times"></div>
+
+    <form action="">
+        <input type="search" name="" placeholder="search here..." id="search-box">
+        <label for="search-box" class="fas fa-search"></label>
+    </form>
+</div>
+
+<!-- footer section starts  -->
+
+<section class="footer" id="contact_me">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="#about">home</a>
+            <a href="#contact_me">contact me</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="tel:+639759843740"> <i class="fas fa-phone"></i> +639759843740 </a>
+            <a href="mailto:robzleesantor@gmail.com"> <i class="fas fa-envelope"></i> robzleesantor@gmail.com </a>
+            <a href="#"> <i class="fas fa-map"></i> nuesa roxas, isabela - 3320 </a>
+        </div>
+
+        <div class="box">
+            <h3>follow me</h3>
+            <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+            <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+            <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+            <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+            <a href="#"> <i class="fab fa-github"></i> github </a>
+        </div>
+
+    </div>
+
+    <div class="credit">copyright <span>2021</span> | all rights reserved!</div>
+
+</section>
+
+<!-- footer section ends -->
+
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
